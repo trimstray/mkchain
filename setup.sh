5ce84577d5c480c895d57c6c6937defb571d642c
trimstray <contact@nslab.at>
2018-01-22 23:06:40 +0100

    setup.sh: minor fixes - signed-off-by: trimstray <contact@nslab.at>

--- setup.sh
+++ setup.sh
@@ -1,20 +1,19 @@
 #!/usr/bin/env bash
 
-# To adapt this script to your project, do the following:
-#   - vim: ESC:%s/multitor/your-project/g
-#   - sed: set -i 's/multitor/your-project/g'
+readonly _dir="$(dirname "$(readlink -f "$0")")"
 
+# shellcheck disable=SC2034
 _arg="$1"
 
 if [[ "$1" == "install" ]] ; then
 
   printf "%s\n" "Create symbolic link to /usr/local/bin"
 
-  if [[ -e "bin/sslmerge" ]] ; then
+  if [[ -e "${_dir}/bin/sslmerge" ]] ; then
 
     if [[ ! -e "/usr/local/bin/sslmerge" ]] ; then
 
-      ln -s bin/sslmerge /usr/local/bin
+      ln -s "${_dir}/bin/sslmerge" /usr/local/bin
 
     fi
 
@@ -22,11 +21,11 @@ if [[ "$1" == "install" ]] ; then
 
   printf "%s\n" "Create man page to /usr/local/man/man8"
 
-  if [[ -e "doc/man8/sslmerge.8" ]] ; then
+  if [[ -e "${_dir}/doc/man8/sslmerge.8" ]] ; then
 
     if [[ ! -e "/usr/local/man/man8/sslmerge.8.gz" ]] ; then
 
-      cp doc/man8/sslmerge.8 /usr/local/man/man8
+      cp "${_dir}/doc/man8/sslmerge.8" /usr/local/man/man8
       gzip /usr/local/man/man8/sslmerge.8
 
     fi
